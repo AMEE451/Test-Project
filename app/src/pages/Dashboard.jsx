5f004d42b0fef083f674275ed6996d1db2d733b1
AMEE451 <desaiamee699@gmail.com>
2025-06-24 17:34:46 +0530

    test-home-booking

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Card from '../components/card';
+import Card from '../components/Card';
 import { homes } from '../data/homes';
 
 const Dashboard = () => {
